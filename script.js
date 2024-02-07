a4b23e4a31a322786b13c533c3305d9d91b771f0
shiblu123p <159017103+shiblu123p@users.noreply.github.com>
2024-02-07 16:18:40 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+// console.log("hello")
+
+// var a = "hello"
+// console.log(a)
+// {
+//     console.log(a)
+
+// }
+
+// console.log("hello")
+
+
+const person = {
+    name:"shiblu",
+    age:"19",
+    place:"koduvally"
+}
+
+console.log(person.name,person.age,person.place)
